8c77f362f6f3eb6a4d6d2307529893c1811a4940
暴虐的小金 <2044538794@qq.cpm>
2022-12-06 15:30:40 +0800

    双页面

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@
  * @Author: 暴虐的小金
  * @Date: 2022-12-06 09:46:31
  * @LastEditors: 暴虐的小金
- * @LastEditTime: 2022-12-06 09:46:41
+ * @LastEditTime: 2022-12-06 11:25:44
  */
-alert('11')
+window.addEventListener('load', function() {
+    alert('11')
+})
