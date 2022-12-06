6a58bd1467bd3faf2ff9138252db6deae2dbc578
暴虐的小金 <2044538794@qq.cpm>
2022-12-06 18:33:58 +0800

    3.0

--- js/index.js
+++ js/index.js
@@ -4,8 +4,9 @@
  * @Author: 暴虐的小金
  * @Date: 2022-12-06 09:46:31
  * @LastEditors: 暴虐的小金
- * @LastEditTime: 2022-12-06 11:25:44
+ * @LastEditTime: 2022-12-06 18:33:15
  */
 window.addEventListener('load', function() {
     alert('11')
+    console.log('789');
 })
